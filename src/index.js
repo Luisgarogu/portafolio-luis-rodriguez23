45ddc847c1ce54280dc2f10bdc34ea20da354ee8
Luisgarogu <luis.gabriel.rodriguez@correounivalle.edu.co>
2023-09-14 15:43:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import Experience from "./Experience";
+import { Canvas } from "@react-three/fiber";
+import "./styles.css"
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <Canvas camera={{ position: [2, 0, 5] }}    >
+        <Experience />
+    </Canvas>
+);
